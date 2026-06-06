5a1c8bc0057ac93ba5e8539382485997018f029d
Maneesha <148954016+mdulmini@users.noreply.github.com>
2026-06-06 16:21:18 +0530

    Initial backend commit

---
+++ src/main/java/com/coaching/coaching_backend/CoachingBackendApplication.java
@@ -0,0 +1,11 @@
+package com.coaching.coaching_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachingBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoachingBackendApplication.class, args);
+    }
+}
